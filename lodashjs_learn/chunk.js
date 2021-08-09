3e6e04becf76a79375358f7c35228403325ac70a
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-09 15:15:23 +0800

    Update chunk.js

--- lodashjs_learn/chunk.js
+++ lodashjs_learn/chunk.js
@@ -1,15 +1,14 @@
 //将数组拆分为二维数组
-//哎呀这样不好，改变了原数组了
 const array = ['a', 'b', 'c', 'd', 'e']
 function chunk(array, size) {
-    if(!Array.isArray(array)||size<=0){
+    if (!Array.isArray(array) || size <= 0) {
         return undefined
     }
     let twoArray = []
     let j = 0
-    while (j< array.length) {
+    while (j < array.length) {
         let temp = []
-        for (let i = 0; i < size && j<array.length; i++) {
+        for (let i = 0; i < size && j < array.length; i++) {
             temp.push(array[j++])
         }
         twoArray.push(temp)
