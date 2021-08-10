4e443335eea210f014e3978dd6ecf08003455939
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-10 18:44:25 +0800

    Update chunk.js

--- lodashjs_learn/chunk.js
+++ lodashjs_learn/chunk.js
@@ -1,20 +1,63 @@
-//将数组拆分为二维数组
-const array = ['a', 'b', 'c', 'd', 'e']
+// 将数组拆分为二维数组
 function chunk(array, size) {
-    if (!Array.isArray(array) || size <= 0) {
-        return undefined
+    const result = [];
+    const length = array.length;
+    let realArray = []
+
+    if (size === 0) {
+        return []
     }
-    let twoArray = []
-    let j = 0
-    while (j < array.length) {
-        let temp = []
-        for (let i = 0; i < size && j < array.length; i++) {
-            temp.push(array[j++])
+
+    switch (Object.prototype.toString.call(array).replace(/^\[object (\S+)\]$/, '$1')) {
+        case "Array":
+            realArray = array
+            break
+        case "String":
+            realArray = array.split()
+            break
+        default:
+            return []
+    }
+
+    let j = 0;
+
+    while (j < length) {
+        let temp = [];
+
+        for (let i = 0; i < size; i++) {
+            temp.push(array[j]);
+            j += 1;
+
+            if (j >= length) {
+                break;
+            }
         }
-        twoArray.push(temp)
+
+        result.push(temp)
     }
-    return twoArray
+
+    console.log(result);
+
+    return result
 }
 
-let testArray = chunk(array, 2)
-console.log(testArray)
+const array = ['a', 'b', 'c', 'd', 'e'];
+const str1 = 'paper';
+const str2 = '123445';
+const func = function () { };
+const obj = { test: 1 }
+
+console.log(chunk(array, 0))
+chunk(array, 1);
+chunk(array, 2);
+chunk(array, 3);
+chunk(array, 4);
+chunk(array, 5);
+chunk(array, 6);
+
+chunk(str1, 2);
+chunk(str2, 2);
+
+console.log(chunk(func, 2))
+
+console.log(chunk(obj, 2))
