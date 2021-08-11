e16fc2a74c21e102a76af6ce484cd37807b4e387
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-11 19:59:36 +0800

    Update drop.js

--- lodashjs_learn/drop.js
+++ lodashjs_learn/drop.js
@@ -1,41 +1,40 @@
+var myType = require("./myType")
+
 //创建一个切片数组，去除array前面的n个元素。（n默认值为1。）
 function dropRight(array, n = 1) {
     let realArray = []
+    n = Number(n) >= 0 ? Number(n) : 0  ////  校验 n 是不是正整数
 
-    switch (Object.prototype.toString.call(array).replace(/^\[object (\S+)\]$/, '$1')) {
-        case "Array":
-            realArray = array
-            break
-        case "String":
-            realArray = array.split("")
-            break
-        default:
-            return []
+    if (Array.isArray(array)) {
+        realArray = array
+    } else if (typeof array === "string") {
+        realArray = array.split("")
+    } else {
+        return []
     }
-
-    n = Number(n)    
-
 
     return realArray.slice(n)
 }
 
-console.log(dropRight([1,2,3],1))
-console.log(dropRight([1,2,3],2))
-console.log(dropRight([1,2,3],5))
-console.log(dropRight([1,2,3]))
+console.log(dropRight([1, 2, 3], 1))
+console.log(dropRight([1, 2, 3], 2))
+console.log(dropRight([1, 2, 3], 5))
+console.log(dropRight([1, 2, 3]))
 console.log(dropRight())
 
-console.log(dropRight("paper",1))
-console.log(dropRight([],1))
-console.log(dropRight(true,1))
-console.log(dropRight({},1))
-console.log(dropRight(function(){},1))
+console.log(dropRight("paper", 1))
+console.log(dropRight([], 1))
+console.log(dropRight(true, 1))
+console.log(dropRight({}, 1))
+console.log(dropRight(function () { }, 1))
 
-console.log(dropRight([1,2,3],""))
-console.log(dropRight([1,2,3],"1"))
-console.log(dropRight([1,2,3],"1,2"))
-console.log(dropRight([1,2,3],{test:1}))
-console.log(dropRight([1,2,3],{}))
-console.log(dropRight([1,2,3],[]))
-console.log(dropRight([1,2,3],[1]))
-console.log(dropRight([1,2,3],[1,2]))
+console.log(dropRight([1, 2, 3], ""))
+console.log(dropRight([1, 2, 3], "1"))
+console.log(dropRight([1, 2, 3], "1,2"))
+console.log(dropRight([1, 2, 3], { test: 1 }))
+console.log(dropRight([1, 2, 3], {}))
+console.log(dropRight([1, 2, 3], []))
+console.log(dropRight([1, 2, 3], [1]))
+console.log(dropRight([1, 2, 3], [1, 2]))
+
+console.log(dropRight([1, 2, 3], -1))
