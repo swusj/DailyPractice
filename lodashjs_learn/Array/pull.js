2fe070126e2b1bd5d2564e898b3171838d74b5c3
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-27 16:31:06 +0800

    .

--- lodashjs_learn/Array/pull.js
+++ lodashjs_learn/Array/pull.js
@@ -1,8 +1,9 @@
 // 移除数组array中所有和给定值相等的元素，使用SameValueZero 进行全等比较。
 function pull(array, ...values) {
-	for (let i in array) {
+	for (let i = 0; i < array.length; i++) {
 		if (values.includes(array[i])) {
 			array.splice(i, 1);
+			i--;
 		}
 	}
 	return array;
--- lodashjs_learn/Array/pullAll.js
+++ lodashjs_learn/Array/pullAll.js
@@ -1,3 +1,15 @@
 // 这个方法类似_.pull，区别是这个方法接收一个要移除值的数组。
 // 不同于_.difference, 这个方法会改变数组 array。
-function pullAll(array, values) {}
+function pullAll(array, values) {
+	for (let i = 0; i < array.length; i++) {
+		if (values.includes(array[i])) {
+			array.splice(i, 1);
+			i--;
+		}
+	}
+	return array;
+}
+
+var array = [1, 2, 3, 1, 2, 3];
+pullAll(array, [2, 3]);
+console.log(array);
